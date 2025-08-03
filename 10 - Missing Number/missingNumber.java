d854ee3b7fd0159d8f1dec1df89f2e98160137be
Paramjeetsinghlohia <139992567+Paramjeet-singh5745@users.noreply.github.com>
2025-08-03 15:11:08 +0530

    Create missingNumber.java

---
+++ 10 - Missing Number/missingNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int range = nums.length;
+
+        int actualSum = (range * (range + 1)) / 2;
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        int ans = actualSum - sum;
+
+        return ans;
+    }
+}
